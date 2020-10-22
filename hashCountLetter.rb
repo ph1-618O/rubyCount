4aa729cf54172fdae2c67b09021a3cb5e5290b43
Jen S <j3n.s3idl@gmail.com>
2020-10-22 18:56:44 -0400

    adding comments

--- hashCountLetter.rb
+++ hashCountLetter.rb
@@ -2,13 +2,15 @@ puts "Text please: "
 text = gets.chomp
 
 #puts text 
-
+#splitting the text by spaces
 words = text.split(" ")
+#creating hashes
 frequencies = Hash.new(0)
 letterCount = Hash.new(0)
 
 #puts words
 
+#populating hashes with data from input
 words.each do |word| 
   frequencies[word] += 1 
     word.each_char do |letter|
@@ -17,18 +19,22 @@ words.each do |word|
   end
   
 #puts letterCount
-
+#sorting hashes by amount dec order
 frequencies = frequencies.sort_by {|a, b| b }
 frequencies.reverse!
+letterCount = letterCount.sort_by {|a, b| b }
+letterCount.reverse!
+
+#printing output key/values for counted words
 puts "---------------------------"
 puts "Word Frequency of Text::"
 puts "---------------------------"
 frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
+
+#printing output key/values for counted chrs
 puts "---------------------------"
 puts "Letter frequency of Text::"
 puts "---------------------------"
-letterCount = letterCount.sort_by {|a, b| b }
-letterCount.reverse!
 letterCount.each { |word, frequency| print word + frequency.to_s + ", " }
 
 #puts letterCount
